81e6aee0f48f4a63d57477295e01cdf6d8429593
xiaofeng.pang <xiaofeng.pang@quanshi.com>
2017-11-22 14:07:07 +0800

    first commit

---
+++ AttachmentModify/Meeting.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace domain.MessageModify
+{
+    class Meeting
+    {
+  
+        private UInt32 GetStamp(DateTime dt)
+        {
+            TimeSpan ts = dt - TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            UInt32 uiStamp = Convert.ToUInt32(ts.TotalSeconds);
+            return uiStamp;
+        }
+
+        private UInt32 GetStamp()
+        {
+            return GetStamp(DateTime.Now);
+        }
+
+
+        private String GetToken()
+        {
+
+            var request = (HttpWebRequest)WebRequest.Create("https://testcloudb.domain.com/ucopenapi/auth/token/create");
+            request.ContentType = "application/json";
+            request.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                string json = "{\"role\":0," +
+                   "\"username\":\"admin@ucb20151104.domain.com\", " +
+                   "\"password\":\"11111111\", " +
+                   "\"appId\":0" +
+                    "}";
+
+                System.Console.WriteLine("请求Token: ");
+                System.Console.WriteLine(json);
+
+                streamWriter.Write(json);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            string result;
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+                System.Console.WriteLine("请求Token响应：");
+                System.Console.WriteLine(result);
+            }
+
+            try
+            {
+                var jObject = JObject.Parse(result);
+
+                if (jObject != null)
+                {
+
+                    var data = jObject["data"];
+                    string token = data["token"].ToString();
+                    Console.WriteLine("Token :" + data["token"].ToString());
+                    return token;
+                }
+                return "";
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+
+        }
+
+        public void Create()
+        {
+
+            System.Console.WriteLine(DateTime.Now.Ticks);
+            System.Console.WriteLine("End");
+
+            var request = (HttpWebRequest)WebRequest.Create("https://testcloudb.domain.com/ucopenapi/calendar/create");
+            request.ContentType = "application/json";
+            request.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                string json = "{\"username\":\"admin@ucb20151104.domain.com\"," +
+                   "\"token\":\"" + GetToken() + "\", " +
+                   "\"data\":{" +
+                   "\"title\":\"meeting test\", " +
+                   "\"location\":\"meeting room\", " +
+                   "\"summary\":\"meeting summary\", " +
+                   "\"owner\":\"testb_sanguo20@domain.com\", " +
+                   "\"members\":[" + "\"admin@ucb20151104.domain.com\", \"test_yunpan3@bee.com\"" + "]," +
+                   "\"startTime\":" + (GetStamp() + 3600 * 3) + ", " +
+                   "\"duration\":20, " +
+                   "\"priority\":1 " +
+                    "}}";
+                System.Console.WriteLine("请求创建日程： ");
+                System.Console.WriteLine(json);
+
+                streamWriter.Write(json);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+
+                System.Console.WriteLine("请求创建日程响应：");
+                System.Console.WriteLine(result.ToString());
+
+                System.Console.WriteLine("End");
+            }
+        }
+    }
+}
---
+++ AttachmentModify/MessageModifier .cs
@@ -0,0 +1,122 @@
+﻿// AttachmentModify  
+// ----------------------------------------------------------  
+// Example for intercepting email messages in an Exchange 2010 transport queue  
+//   
+// The example intercepts messages sent from a configurable email address(es)  
+// and checks the mail message for attachments have filename in to format  
+//   
+//      WORKBOOK_{GUID}  
+//  
+// Changing the filename of the attachments makes it easier for the information worker  
+// to identify the reports in the emails and in the file system as well.  
+//  
+// ----------------------------------------------------------  
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml;
+
+// the lovely Exchange   
+using Microsoft.Exchange.Data.Transport;
+using Microsoft.Exchange.Data.Transport.Smtp;
+using Microsoft.Exchange.Data.Transport.Email;
+using Microsoft.Exchange.Data.Transport.Routing;
+
+namespace QuanShi.MessageModify
+{
+    #region Message Modifier Factory
+
+    /// <summary>  
+    /// Message Modifier Factory  
+    /// </summary>  
+    public class MessageModifierFactory : RoutingAgentFactory
+    {
+
+        public override RoutingAgent CreateAgent(SmtpServer server)
+        {
+            return new MessageModifier();
+        }
+    }  
+
+    #endregion
+
+    #region Message Modifier Routing Agent
+
+    /// <summary>  
+    /// The Message Modifier Routing Agent for modifying an email message  
+    /// </summary>  
+    public class MessageModifier : RoutingAgent
+    {
+        // The agent uses the fileLock object to synchronize access to the log file  
+        private object fileLock = new object();
+
+        /// <summary>  
+        /// The current MailItem the transport agent is handling  
+        /// </summary>  
+        private MailItem mailItem;
+
+        /// <summary>  
+        /// This context to allow Exchange to continue processing a message  
+        /// </summary>  
+        private AgentAsyncContext agentAsyncContext;
+
+        public MessageModifier()
+        {
+            this.OnRoutedMessage += OnRoutedMessageHandler;
+        }  
+  
+
+        /// <summary>  
+        /// Event handler for OnRoutedMessage event  
+        /// </summary>  
+        /// <param name="source">Routed Message Event Source</param>  
+        /// <param name="args">Queued Message Event Arguments</param>  
+        void OnRoutedMessageHandler(RoutedMessageEventSource source, QueuedMessageEventArgs args)
+        {
+            lock (fileLock)
+            {
+                try
+                {
+                    this.mailItem = args.MailItem;
+                    this.agentAsyncContext = this.GetAgentAsyncContext();  
+
+                    foreach(EmailRecipient rec in this.mailItem.Message.To)
+                    {
+                        if (rec.NativeAddress == "ex10test04@hitest10.com")
+                        {
+                            Meeting meeting = new Meeting();
+                            meeting.Create();
+                        }
+                    }
+                          
+                          
+                }
+                catch (System.IO.IOException ex)
+                {
+                   
+                    Debug.WriteLine(ex.ToString());
+                    this.agentAsyncContext.Complete();
+                }
+                finally
+                {
+                    // We are done  
+                    this.agentAsyncContext.Complete();
+                }
+            }
+
+            // Return to pipeline  
+            return;
+        }
+
+    }
+
+    #endregion
+
+}
